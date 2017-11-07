ff78bdf421d127da7e7f805990bbe84e7b18338d
Wilfredo Fermin Reyes <wilfredoferminreyes@gmail.com>
2017-11-06 20:58:43 -0400

    Aplicacion base

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.master')
+@section('title','Bienvendio BSConttrol')
+@section('class-page','tutorial-page')
+@section('class-page','section landing-section')
+@section('class-page','section text-center section-landing')
+@section('content')
+
+    <div class="header header-filter" style="background-image: url('{{asset('img/bgbs.jpg')}}');">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="title">BSCONTROL</h1>
+                    <h5 class="description">Sistema de servicios variados</h5>
+                    <a href="#" id="tourTutorial" class="btn btn-danger btn-raised btn-lg" >
+                        <i class="fa fa-play" ></i> Como funciona?
+                    </a>
+                    <a href="#" id="tourIntro" class="btn btn-success btn-raised btn-lg" >
+                        <i class="fa fa-bus"></i> Inicia un demo
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="main main-raised">
+        <div class="section">
+            <div class="section text-center section-landing">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="info">
+                            <div class="icon icon-primary">
+                                <i class="material-icons">chat</i>
+                            </div>
+                            <h4 class="info-title">First Feature</h4>
+                            <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="info">
+                            <div class="icon icon-success">
+                                <i class="material-icons">verified_user</i>
+                            </div>
+                            <h4 class="info-title">Second Feature</h4>
+                            <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="info">
+                            <div class="icon icon-danger">
+                                <i class="material-icons">fingerprint</i>
+                            </div>
+                            <h4 class="info-title">Third Feature</h4>
+                            <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="section landing-section">
+                    <hr width="50%">{{--Linea separador--}}
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <h1><span class="tim-note">Estamos trabajamos para ti.</span></h1>
+                            <h4 class="text-center description">Queremos ayudarte,no dejes de enviarnos tus inquietudes.</h4>
+                            <form class="contact-form">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group label-floating is-empty">
+                                            <label class="control-label">Tu nombre completo</label>
+                                            <input type="email" class="form-control">
+                                            <span class="material-input"></span></div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group label-floating is-empty">
+                                            <label class="control-label">Tu correo electronico</label>
+                                            <input type="email" class="form-control">
+                                            <span class="material-input"></span></div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group label-floating is-empty">
+                                    <label class="control-label">Tu mensaje</label>
+                                    <textarea class="form-control" rows="4"></textarea>
+                                    <span class="material-input"></span></div>
+
+                                <div class="row">
+                                    <div class="col-md-4 col-md-offset-4 text-center">
+                                        <button class="btn btn-primary btn-raised">
+                                            Enviar mensaje
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,166 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/apple-icon.png')}}">
+    <link rel="icon" type="image/png" href="{{asset('img/favicon.png')}}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>@yield('title','App Shop')</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <!--     Fonts and icons     -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+    <!-- CSS Files -->
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/material-kit.css')}}" rel="stylesheet"/>
+
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="{{asset('css/demo.css')}}" rel="stylesheet" />
+<body class="@yield('class-page')">
+
+
+<nav class="navbar navbar-transparent navbar-fixed-top navbar-color-on-scroll" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-index">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="{{url('/app')}}" id="tourInicio">
+                <div class="logo-container" >
+                    <div class="logo">
+                        <img src="{{asset('img/logobs.jpg')}}"  rel="tooltip" title=BSCONTROL data-placement="bottom" data-html="true" width="48" height="48">
+                    </div>
+                    <div class="brand">
+                        BODYSHOP CONTROL
+                    </div>
+                </div>
+            </a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse">
+            <ul  class="nav navbar-nav navbar-right">
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="#" class="btn btn-primary btn-round" data-toggle="modal" data-target="#myModal">
+                                <i class="material-icons">verified_user</i> Acceso
+                            </a>
+                            <a href="{{ route('register') }}" class="btn btn-primary btn-round">
+                                <i class="material-icons">person_add</i> Registrate
+                            </a>
+                            @endauth
+                    </div>
+                @endif
+            </ul>
+
+
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<div class="wrapper">
+    @yield('content')
+</div>
+<footer class="footer footer-transparent">
+    <div class="container">
+        <nav class="pull-right">
+            <ul>
+                <li>
+                    <a href="https://wilfredofermin.github.io/">
+                        <i class="material-icons">web</i> WF Developers
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!--
+        <div class="social-area pull-right">
+            <a class="btn btn-social btn-twitter btn-just-icon" href="https://twitter.com/CreativeTim">
+                <i class="fa fa-twitter"></i>
+            </a>
+            <a class="btn btn-social btn-facebook btn-just-icon" href="https://www.facebook.com/CreativeTim">
+                <i class="fa fa-facebook-square"></i>
+            </a>
+            <a class="btn btn-social btn-google btn-just-icon" href="https://plus.google.com/+CreativetimPage">
+                <i class="fa fa-google-plus"></i>
+            </a>
+        </div>
+        -->
+        <div class="copyright">
+            &copy; 2017 Bodyshop Athletic Club
+        </div>
+    </div>
+</footer>
+
+</body>
+<!--   Core JS Files   -->
+<script src="{{asset('js/jquery.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('js/material.min.js')}}"></script>
+
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="{{asset('js/nouislider.min.js')}}" type="text/javascript"></script>
+
+<!--  Plugin for the Datepicker, full documentation here: http://www.eyecon.ro/bootstrap-datepicker/ -->
+<script src="{{asset('js/bootstrap-datepicker.js')}}" type="text/javascript"></script>
+
+<!-- Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc -->
+<script src="{{asset('js/material-kit.js')}}" type="text/javascript"></script>
+<!-- Inicio Modal -->
+@include('modal.acceso')
+        <!-- Fin Modal -->
+<script>
+    $(function(){
+        $('#tourIntro').click(function(){
+            Tour.run([
+                {
+                    element: $('#tourAcceso'),
+                    content: 'Debes tener una cuenta para acceder al sistema de comprar',
+                    position: 'bottom'bienvenido.blade.php
+                },
+                {
+                    element: $('#tourRegistro'),
+                    content: 'De no tener una cuenta, accede desde aqui',
+                    position: 'bottom'
+                },
+
+                {
+                    element: $('#tourMsg'),
+                    content: 'Para alguna informacion de interes, escribe un mensaje',
+                    position: 'top'
+                },
+                {
+                    element: $('#tourTutorial'),
+                    content: 'Mira el tutorial de funcionamiento basico',
+                    position: 'bottom'
+                }
+            ],{language:"es"});
+        });
+    });
+</script>
+
+<script>
+
+    $().ready(function(){
+
+        $(window).on('scroll', materialKit.checkScrollForTransparentNavbar);
+
+    });
+
+</script>
+
+</html>
