983adcb7b0c26b46401e3e1d00e51ab9dcffc7e0
Wilfredo Fermin Reyes <wilfredoferminreyes@gmail.com>
2017-11-08 22:36:26 -0400

    Imagen backgroun Templeta Admin

--- resources/views/layouts/templete.blade.php
+++ resources/views/layouts/templete.blade.php
@@ -1,20 +1,13 @@
 <!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/apple-icon.png')}}" />
-    <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-    <title>@yield('title','BSCONTROL')</title>
-
-
+<html lang="es">
 <head>
     <meta charset="utf-8">
     <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/apple-icon.png')}}">
     <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Material Dashboard by Creative Tim</title>
+
+    <title>@yield('title','BSCONTROL')</title>
+
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
     <meta name="viewport" content="width=device-width">
     <!-- Bootstrap core CSS     -->
@@ -23,6 +16,8 @@
     <link href="{{asset('assets/css/material-dashboard.css?v=1.2.0')}}" rel="stylesheet">
     <!--  CSS for Demo Purpose, don't include it in your project     -->
     <link href="{{asset('assets/css/demo.css')}}" rel="stylesheet">
+    <!-- SweetAlert2 |https://cdn.jsdelivr.net/sweetalert2/6.6.2/sweetalert2.css -->
+    <link rel="stylesheet" href="{{asset('css/sweetalert2.css')}}">
     <!--     Fonts and icons     -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <link href="http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons" rel="stylesheet" type="text/css">
@@ -41,7 +36,7 @@
 <body>
 
 <div class="wrapper">
-    <div class="sidebar" data-color="purple" data-image="{{asset('assets/img/sidebar-1.jpg')}}">
+    <div class="sidebar" data-color="purple" data-image="{{asset('img/basket.jpg')}}">
 
         <!--
             Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
@@ -58,6 +53,7 @@
         </div>
 
         <div class="sidebar-wrapper">
+
             <ul class="nav">
                 <li class="active">
                     <a href="{{url('/home')}}">
@@ -65,8 +61,16 @@
                         <p>Dashboard</p>
                     </a>
                 </li>
+                <li>
+                    <a href="{{url('/home/solicitud')}}">
+                        <i class="material-icons">assignment_turned_in</i>
+                        <p>Solicitudes</p>
+                    </a>
+                </li>
             </ul>
         </div>
+
+
     </div>
 
     <div class="main-panel">
@@ -138,7 +142,7 @@
 
                     <form class="navbar-form navbar-right" role="search">
                         <div class="form-group  is-empty">
-                            <input type="text" class="form-control" placeholder="Search">
+                            <input type="text" class="form-control" placeholder="Buscar..">
                             <span class="material-input"></span>
                         </div>
                         <button type="submit" class="btn btn-white btn-round btn-just-icon">
@@ -162,19 +166,54 @@
 </body>
 
 <!--   Core JS Files   -->
+<script src="../assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+<script src="../assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="../assets/js/material.min.js" type="text/javascript"></script>
+<!--  Charts Plugin -->
+<script src="../assets/js/chartist.min.js"></script>
+<!--  Dynamic Elements plugin -->
+<script src="../assets/js/arrive.min.js"></script>
+<!--  PerfectScrollbar Library -->
+<script src="../assets/js/perfect-scrollbar.jquery.min.js"></script>
+<!--  Notifications Plugin    -->
+<script src="../assets/js/bootstrap-notify.js"></script>
+
+<!-- Material Dashboard javascript methods -->
+<script src="../assets/js/material-dashboard.js?v=1.2.0"></script>
+<!-- Material Dashboard DEMO methods, don't include it in your project! -->
+<script src="../assets/js/demo.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+
+        // Javascript method's body can be found in assets/js/demos.js
+        demo.initDashboardPageCharts();
+
+    });
+</script>
+
+<!--   Core JS Files   -->
 <script src="{{asset('assets/js/jquery-3.1.0.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('assets/js/bootstrap.min.js')}}" type="text/javascript"></script>
 <script src="{{asset('assets/js/material.min.js')}}" type="text/javascript"></script>
 
+
+
 <!--  Charts Plugin -->
 <script src="{{asset('assets/js/chartist.min.js')}}"></script>
 
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
 <!--  Notifications Plugin    -->
 <script src="{{asset('assets/js/bootstrap-notify.js')}}"></script>
-
-
+<!--  Dynamic Elements plugin -->
+<script src="{{asset('assets/js/arrive.min.js')}}"></script>
+<!--  PerfectScrollbar Library -->
+<script src="{{asset('assets/js/perfect-scrollbar.jquery.min.js')}}"></script>
 
 <!--  Google Maps Plugin    -->
+{{--SweeAlert2 | https://cdn.jsdelivr.net/sweetalert2/6.6.2/sweetalert2.js --}}
+<script src="{{asset('js/sweetalert2.js')}}"></script>
 
 <!-- Material Dashboard javascript methods -->
 <script src="{{asset('assets/js/material-dashboard.js')}}"></script>
@@ -182,5 +221,13 @@
 <!-- Material Dashboard DEMO methods, don't include it in your project! -->
 <script src="{{asset('assets/js/demo.js')}}"></script>
 
+<script type="text/javascript">
+    $(document).ready(function(){
+
+        // Javascript method's body can be found in assets/js/demos.js
+        demo.initDashboardPageCharts();
+
+    });
+</script>
 
 </html>
